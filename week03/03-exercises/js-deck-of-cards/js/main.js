9bda6c25e7da319829187176136dacdb3f4810ea
Michael Herman <hermanmu@gmail.com>
2015-07-19 16:14:09 -0600

    updated week 3, day 1

--- week03/03-exercises/js-deck-of-cards/js/main.js
+++ week03/03-exercises/js-deck-of-cards/js/main.js
@@ -1,18 +1,22 @@
-var options = {
-	containerId 		: 'table',
-	drawTimeInterval 	: 50
-};
 
+// create deck
+
+var options = {
+  containerId: 'table',
+  drawTimeInterval: 50
+};
 var deck = new Deck(options);
 
 
+// event handlers
+
 document.getElementById('draw-button').onclick = function () {
-	deck.draw(1);
+  deck.draw(1);
 };
 document.getElementById('draw-all-button').onclick = function () {
-	deck.drawAll();
+  deck.drawAll();
 };
 document.getElementById('reset-deck').onclick = function () {
-	deck.destroy();
-	deck = new Deck(options);
+  deck.destroy();
+  deck = new Deck(options);
 };

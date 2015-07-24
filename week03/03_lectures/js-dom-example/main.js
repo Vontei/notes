daa8ab7b91de90e89376916b6716204403b45b3c
Michael Herman <hermanmu@gmail.com>
2015-07-24 02:52:11 -0600

    updaed js-dom-example notes

--- week03/03_lectures/js-dom-example/main.js
+++ week03/03_lectures/js-dom-example/main.js
@@ -3,9 +3,9 @@ var selectBox = document.getElementsByTagName("select")[0];
 
 // event handler
 selectBox.addEventListener("click", function() {
-  // grab selected items
+  // grab selected items using the `selectedOptions()` property
   var getSelectedOptions = selectBox.selectedOptions;
-  // iterate through each option and log it to the console
+  // iterate through the selected options and log each to the js console
   for (var i = 0; i < getSelectedOptions.length; i++) {
     console.log(getSelectedOptions[i]);
   }

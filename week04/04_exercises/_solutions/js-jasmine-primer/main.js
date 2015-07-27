ffc2a3d08bf674f2cc92b102f80fdfee3217f526
reydelo <reyna.deloge@gmail.com>
2015-07-27 15:05:31 -0600

    complete test activities

--- week04/04_exercises/_solutions/js-jasmine-primer/main.js
+++ week04/04_exercises/_solutions/js-jasmine-primer/main.js
@@ -5,7 +5,42 @@ exports.outputHelloWorld = function () {
 exports.calculate = function (num) {
   if (num <= 10) {
     return num * .1;
-  } else {
+  } else if (num <= 20) {
     return (num - 10) * .07 + 1;
+  } else if (num <= 30) {
+    return (num - 20) * .05 + 1 + .7;
+  } else {
+    return (num - 30) * .03 + 1 + .7 + .5;
   }
+}
+
+exports.leapYear = function (year) {
+  if (year % 4 == 0) {
+    if (year % 100 == 0 && year % 400 != 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+exports.changeToUpper = function (array) {
+  for (i in array) {
+    array[i] = array[i].toUpperCase();
+  }
+  return array;
+}
+
+exports.findGreatestStrength = function (list) {
+  var greatest = 0;
+  var strongest = {};
+  for (i in list) {
+    if (list[i].strength > greatest) {
+      greatest = list[i].strength;
+      strongest = list[i];
+    }
+  }
+  return strongest;
 }
--- week04/04_exercises/_solutions/js-jasmine-primer/spec/spec.js
+++ week04/04_exercises/_solutions/js-jasmine-primer/spec/spec.js
@@ -19,4 +19,38 @@ describe('Tax Calculator', function(){
     expect(code.calculate(15)).toEqual(1.35);
   });
 
+  it('should tax 5% on the third $10', function(){
+    expect(code.calculate(25)).toEqual(1.95);
+  });
+
+  it('should tax 5% on the third $10', function(){
+    expect(code.calculate(40)).toEqual(2.50);
+  });
+
+});
+
+describe('Leap Year', function(){
+
+  it('should return true if leap year', function(){
+    expect(code.leapYear(2012)).not.toBe(false);
+    expect(code.leapYear(2015)).toBe(false);
+    expect(code.leapYear(1700)).toBe(false);
+  });
+
+});
+
+describe('Change to Upper', function() {
+
+  it('should return an array of uppercase strings', function(){
+    expect(code.changeToUpper(['red', 'green', 'blue'])).toEqual(['RED', 'GREEN', 'BLUE']);
+  });
+
+});
+
+describe('Find Greatest Strength', function() {
+
+  it('should return the object with the greatest strength attribute', function(){
+    expect(code.findGreatestStrength([{strength: 20, name: 'A'}, {strength: 10, name: 'B'}])).toEqual({strength: 20, name: 'A'});
+  });
+
 });

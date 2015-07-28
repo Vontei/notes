02c2ecceb7421618f4ca0b2f9dfa7ae1725ef455
Michael Herman <hermanmu@gmail.com>
2015-07-28 10:44:58 -0600

    updated tip solution

--- week03/03_exercises/_solutions/js-tip-calculator/js/main.js
+++ week03/03_exercises/_solutions/js-tip-calculator/js/main.js
@@ -1,15 +1,28 @@
-// add scripts
+// var calcBtn = document.getElementById('calculate-total');
+// var result = document.getElementById('result');
+// var optionList = document.getElementById('tip-percentage');
+// // var tipAmount = optionList.selectedOptions[0].value;
 
-var calcBtn = document.getElementById('calculate-total');
-var result = document.getElementById('result');
-var optionList = document.getElementById('tip-percentage');
-var tipAmount = optionList.selectedOptions[0].value;
+// calcBtn.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   result.innerHTML = "";
+//   var tipAmount = optionList.selectedOptions[0].value;
+//   var calcAmount = document.getElementById('dollar-amount').value;
+//   // console.log(calcBtn)
+//   result.innerHTML = "You should tip $" + Number(calcAmount * tipAmount).toFixed(2);
+// });
 
-calcBtn.addEventListener('click', function(event) {
-  event.preventDefault();
-  result.innerHTML = "";
-  var tipAmount = optionList.selectedOptions[0].value;
-  var calcAmount = document.getElementById('dollar-amount').value;
-  console.log(calcBtn)
-  result.innerHTML = "You should tip $" + Number(calcAmount * tipAmount).toFixed(2);
-});
+$(document).ready(function() {
+
+  $('#calculate-total').on('click', function(event){
+    event.preventDefault();
+
+    var $dollarAmount = $('#dollar-amount').val();
+    var $tipPercentage = $('#tip-percentage').val();
+    var result = ($dollarAmount * $tipPercentage).toFixed(2);
+
+    $('#result').html(result);
+
+  })
+
+})

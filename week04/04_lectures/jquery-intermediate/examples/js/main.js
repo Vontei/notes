17912ad76f1cf4fc6fe9211ee35402c9074d5fc8
Michael Herman <hermanmu@gmail.com>
2015-07-29 14:22:29 -0600

    updates

--- week04/04_lectures/jquery-intermediate/examples/js/main.js
+++ week04/04_lectures/jquery-intermediate/examples/js/main.js
@@ -2,6 +2,49 @@
 
 $(document).on('ready', function() {
 
-  console.log('sanity check!');
+  // console.log('sanity check!');
+
+  var heading = $('#first-heading');
+  // console.log(heading);
+
+  heading.append('yo');
+
+  // var test = $('.container p').first();
+  $('p:eq(3)').append('&nbsp;&nbsp;<span>appending</span>');
+
+  var testClone = heading.clone();
+  var testClone2 = $('#delegate');
+
+  // console.log(testClone2.clone());
+
+  // dom traversal
+
+  $('button').on('click', function(){
+    // console.log("is this working?");
+    // console.log(this);
+    // console.log($(this).css('background', 'red'));
+    // console.log($(this).siblings());
+    // console.log($(this).closest('.container').find('h1'));
+  });
+
+  // event delegation
+
+  $('#delegate').on('click', '#minor-btn', function(){
+    $('#delegate').append('<p>text</p>');
+  });
+
+  $(document).on('click', 'p', function(){
+    console.log("test");
+    // $(this).remove();
+  });
 
 });
+
+
+
+
+
+
+
+
+

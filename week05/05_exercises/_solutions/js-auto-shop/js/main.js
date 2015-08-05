72535b47ed610eb533edb4cea6bc084af4bee0ea
reydelo <reyna.deloge@gmail.com>
2015-08-05 15:05:00 -0600

    car class constructor

--- week05/05_exercises/_solutions/js-auto-shop/js/main.js
+++ week05/05_exercises/_solutions/js-auto-shop/js/main.js
@@ -1,3 +1,64 @@
-// add scripts
+function Car(make, model, year, color) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.state = 'off';
+  this.current_owner = 'manufacturer';
+  this.previous_owners = [];
+  this.passengers = []
+}
+Car.prototype.sale = function(current_owner) {
+  this.previous_owners.push(this.current_owner);
+  this.current_owner = current_owner;
+};
 
-console.log("sanity check!");
+Car.prototype.paint = function(color) {
+  this.color = color;
+}
+
+Car.prototype.start = function() {
+  this.state = "on";
+}
+
+Car.prototype.off = function() {
+  this.state = "off";
+}
+
+Car.prototype.drivesTo = function(destination) {
+  if (this.state == "on") {
+    console.log("Driving to " + destination);
+  } else {
+    console.log("Sorry, this car is off");
+  }
+}
+
+Car.prototype.park = function() {
+  if (this.state == "off") {
+    console.log("Parked!");
+  } else {
+    console.log("Sorry, the car is still on");
+  }
+}
+
+Car.prototype.pickup = function(name) {
+  if (this.state == "on") {
+    this.passengers.push(name);
+    console.log("Driving to pick up " + name)
+  } else {
+    console.log("Sorry, the car is still off");
+  }
+}
+
+Car.prototype.dropoff = function(name) {
+  var index = this.passengers.indexOf(name)
+  if (this.state !== "on") {
+    console.log("Sorry, this car is off");
+  }
+  if (index != -1) {
+    this.passengers.splice(index, 1)
+    console.log(name + " has been dropped off");
+  } else {
+    console.log(name + " was never picked up");
+  }
+}

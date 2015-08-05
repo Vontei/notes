44c36f6f7876126fac8303ef152c3ff1382f7818
Michael Herman <hermanmu@gmail.com>
2015-08-05 12:51:44 -0600

    updated rock paper scissors

--- week05/05_exercises/_solutions/js-rock-paper-scissors/js/main.js
+++ week05/05_exercises/_solutions/js-rock-paper-scissors/js/main.js
@@ -1,29 +1,45 @@
-var Player = function(name) {
-  this.name = name;
+
+// Player constructor
+var Player = function(name){
+  this.name=name;
 };
 
-var Game = function(player1, player2) {
-  this.player1 = player1;
-  this.player2 = player2;
+Player.prototype.picks = function(choice) {
+  if(choice===undefined)
+    return this.choice;
+  this.choice = choice;
 };
 
-Player.prototype.picks = function(pick) {
-  this.pick = pick;
-};
-
-Game.prototype.PAIRS = {
-  'rock': 'scissors',
-  'paper': 'rock',
-  'scissors': 'paper'
+// Game constructor
+var Game = function(p1,p2){
+  this.player1 = p1;
+  this.player2 = p2;
 };
 
 Game.prototype.winner = function() {
-  if (this.player1.pick === this.player2.pick) {
+  if(this.player1.picks() === this.player2.picks()){
     return null;
   }
-  if (this.PAIRS[this.player1.pick] === this.player2.pick) {
-    return this.player1;
-  } else {
+  if(this.player1.picks() === 'rock'){
+    if(this.player2.picks() === 'scissors')
+      return this.player1;
     return this.player2;
   }
+
+  if(this.player1.picks() === 'scissors'){
+    if(this.player2.picks() === 'rock')
+      return this.player2;
+    return this.player1;
+  }
+  if(this.player1.picks() === 'paper'){
+    if(this.player2.picks() === 'rock')
+      return this.player1;
+    return this.player2;
+  }
+
+};
+
+module.exports = {
+  Player : Player,
+  Game : Game
 };

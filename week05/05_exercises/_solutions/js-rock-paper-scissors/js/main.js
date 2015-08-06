37b1e0b3f17147a1016b930cc2f03d03649a5a60
Michael Herman <hermanmu@gmail.com>
2015-08-06 16:00:00 -0600

    updated game library, rock paper scissors solution

--- week05/05_exercises/_solutions/js-rock-paper-scissors/js/main.js
+++ week05/05_exercises/_solutions/js-rock-paper-scissors/js/main.js
@@ -5,8 +5,6 @@ var Player = function(name){
 };
 
 Player.prototype.picks = function(choice) {
-  if(choice===undefined)
-    return this.choice;
   this.choice = choice;
 };
 
@@ -17,22 +15,22 @@ var Game = function(p1,p2){
 };
 
 Game.prototype.winner = function() {
-  if(this.player1.picks() === this.player2.picks()){
+  if(this.player1.choice === this.player2.choice){
     return null;
   }
-  if(this.player1.picks() === 'rock'){
-    if(this.player2.picks() === 'scissors')
+  if(this.player1.choice === 'rock'){
+    if(this.player2.choice === 'scissors')
       return this.player1;
     return this.player2;
   }
 
-  if(this.player1.picks() === 'scissors'){
-    if(this.player2.picks() === 'rock')
+  if(this.player1.choice === 'scissors'){
+    if(this.player2.choice === 'rock')
       return this.player2;
     return this.player1;
   }
-  if(this.player1.picks() === 'paper'){
-    if(this.player2.picks() === 'rock')
+  if(this.player1.choice === 'paper'){
+    if(this.player2.choice === 'rock')
       return this.player1;
     return this.player2;
   }

5aa521367ff2fb13216d84ab8b5ea503f271f116
Michael Herman <hermanmu@gmail.com>
2015-08-07 10:26:11 -0600

    added js oop basics lecture

--- week05/05_lectures/js-oop-basics-review/js/main.js
+++ week05/05_lectures/js-oop-basics-review/js/main.js
@@ -8,3 +8,58 @@
     - a property for image url
     - a create method that renders the image to the DOM via jQuery.
 */
+
+// Media constructor
+var Media = function(name) {
+  this.name = name;
+};
+
+Media.prototype.view = function(){
+  console.log('Viewing ' + this.name);
+};
+
+// Testing Media
+var myMedia = new Media('some media name');
+myMedia.view();
+
+// Picture Constructor
+var Picture = function(name, imageURL) {
+  Media.call(this, name);
+  this.imageURL = imageURL;
+};
+
+Picture.prototype = new Media();
+
+Picture.prototype.render = function() {
+  this.$element = $('<img>')
+    .attr('src', this.imageURL);
+  return this.$element;
+};
+
+// Testing Picture
+var myPicture = new Picture('kitten', 'http://www.tehcute.com/pics/201109/bow-tie-kitten.jpg');
+var myCat = new Picture('cat', 'https://www.petfinder.com/wp-content/uploads/2012/11/99950237-why-cats-meow-632x475.jpg');
+// console.log(myPicture);
+
+$(document).on('ready', function() {
+  $('div').append(myPicture.render());
+  $('div').append(myCat.render());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

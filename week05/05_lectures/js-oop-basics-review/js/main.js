79dac38581312cfe3b9348f163f64a256ba67884
Michael Herman <hermanmu@gmail.com>
2015-08-07 08:42:34 -0600

    added js-basics-review lecture

---
+++ week05/05_lectures/js-oop-basics-review/js/main.js
@@ -0,0 +1,10 @@
+/*
+  CHALLENGE:
+
+  Create a Media class that has -
+    - a name property
+    - a view method that logs "Viewing <media name>" to the console
+  Create a Picture class that extends from Media and adds -
+    - a property for image url
+    - a create method that renders the image to the DOM via jQuery.
+*/
